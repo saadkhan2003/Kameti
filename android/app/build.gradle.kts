30e6440b9e4cd122d46d4b3f973e32c38f68547f
saadkhan2003 <msaad.official6@gmail.com>
2025-12-20 21:08:20 +0500

    build: Standardize APK filename generation and update release metadata for version 1.1.1.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,6 +41,14 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+
+    applicationVariants.all {
+        val variant = this
+        variant.outputs.all {
+            val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
+            output.outputFileName = "Committee-${variant.versionName}.apk"
+        }
+    }
 }
 
 dependencies {
